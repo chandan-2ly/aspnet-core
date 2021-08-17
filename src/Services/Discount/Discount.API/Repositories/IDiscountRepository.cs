79e3f23ac34d8a058f3dbc529102d56413ea5f11
Chandan <Chandansah625@gmail.com>
2021-08-17 08:57:11 +0530

    Added migration to discount microservice

--- src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -9,6 +9,7 @@ namespace Discount.API.Repositories
     public interface IDiscountRepository
     {
         Task<Coupon> GetDiscount(string productName);
+        Task<Coupon> GetAllDiscounts();
         Task<bool> CreateDiscount(Coupon coupon);
         Task<bool> UpdateDiscount(Coupon coupon);
         Task<bool> DeleteDiscount(string productName);
