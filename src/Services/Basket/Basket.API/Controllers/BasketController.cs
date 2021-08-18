aec95eda5713a5c80263eebf7e4e9e1514c86829
Chandan <Chandansah625@gmail.com>
2021-08-18 09:03:01 +0530

    Added grpc client to Baslet microservice and dockerized the discount.grpc microservice

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,10 +13,12 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _basketRepository;
+        private readonly DiscountGrpcService _discountGrpcService;
 
-        public BasketController(IBasketRepository basketRepository)
+        public BasketController(IBasketRepository basketRepository, DiscountGrpcService discountGrpcService)
         {
             _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+            _discountGrpcService = discountGrpcService;
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -30,6 +33,13 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            //Calculate latest price of prodcut into shopping cart after deducting discount if any.
+            foreach(var item in basket.Items)
+            {
+                var couponModel = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= couponModel.Amount;
+            }
+
             return Ok(await _basketRepository.UpdateBasket(basket));
         }
 
